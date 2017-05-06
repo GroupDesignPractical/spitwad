b45130e1fba49da78e676e6b472556ef9777b19a
Nick Hu <me@nickhu.co.uk>
2017-05-06 18:32:32 +0100

    Use rawSql for faster query

--- src/App.hs
+++ src/App.hs
@@ -19,7 +19,6 @@ import Control.Monad.Except
 import Control.Monad.Logger
 import Control.Monad.Reader
 import Control.Monad.Trans.Control
-import Data.List
 import Data.Maybe
 import Data.String.Conversions
 import Data.Time
@@ -166,10 +165,14 @@ server = getStocks :<|> getTrendSources :<|> getNewsSources
                              -> Maybe UTCTime -> Maybe UTCTime -- start, end
                              -> App TrendDataInterval
         getTrendDataInterval (Just tsname) (Just s) (Just e) = do
-          selected <- runDb $ selectList
-            [TrendDataTrend_source_name ==. tsname
-           , TrendDataDate >. s, TrendDataDate <. e]
-            [Asc TrendDataDate, Desc TrendDataVolume]
+          selected <- runDb $ rawSql
+            "SELECT ?? \
+            \FROM trend_data \
+            \WHERE trend_source_name == ? AND date > ? AND date < ? \
+            \GROUP BY date \
+            \HAVING max(volume) \
+            \ORDER BY date ASC;"
+            [PersistText tsname, PersistUTCTime s, PersistUTCTime e]
           let points = entityVal <$> selected
           pure TrendDataInterval
             {
@@ -179,7 +182,7 @@ server = getStocks :<|> getTrendSources :<|> getNewsSources
                 , end = points ^? _last . trendDataDate
                 , splits = length selected
                 }
-            , trendData = mostPopular points
+            , trendData = points
             }
         getTrendDataInterval (Just _) _ _ = throwError
           err400 { errBody = "Bad Request, invalid dates" }
@@ -303,9 +306,6 @@ server = getStocks :<|> getTrendSources :<|> getNewsSources
                     $(logInfo) $ "\x1b[34mGot sentiment " <> cs (show average)
                                     <> " for " <> trend <> "\x1b[0m"
                     pure $ td & trendDataSentiment .~ average
-
-mostPopular :: [TrendData] -> [TrendData]
-mostPopular = nubBy (\td td' -> td ^. trendDataDate == td' ^. trendDataDate)
 
 initialiseDb :: (MonadLogger m, MonadReader Config m, MonadIO m,
                  MonadBaseControl IO m) => FilePath -> m ()
